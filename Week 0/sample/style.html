2a8fdf67b8b5f585185b862a40334f38a7463759
YaelBecerril <138843123+YaelBecerril@users.noreply.github.com>
2023-07-30 22:40:22 -0600

    Se suben archivos.
    Se añaden carpetas de ejercios y archivos hasta la semana 4 de un total de 32 semanas.

---
+++ Week 0/sample/style.html
@@ -0,0 +1,3 @@
+Hello
+
+Test with vi editor
---
+++ Week 2/functions-objects/ball.js
@@ -0,0 +1,26 @@
+var length = data.length;
+
+// you work goes here
+// -----------------------
+// Variables para el código
+
+var dataCounter = -1
+
+//Función que creará las bolitas
+
+
+
+//Loop para crear bolitas
+
+while (dataCounter <= 1)
+{
+    dataCounter++;
+    data[dataCounter];
+    let x = data[dataCounter].x;
+    let y = data[dataCounter].y;
+    let color = data[dataCounter].color;
+    var ball = data [400];
+    create (ball.x, ball.y, ball.color);
+   console.log(create);
+
+}
